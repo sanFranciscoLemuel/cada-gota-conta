72c65910993b6c4b27db56f1ee781a0a3458a59e
Lemuel <lemuel.fisica@gmail.com>
2025-12-09 22:03:03 -0300

    Mapa usando folium

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import folium
+from streamlit_folium import st_folium
 
-st.title('Hi')
+st.title('Projeto Cada Gota Conta')
+
+
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
